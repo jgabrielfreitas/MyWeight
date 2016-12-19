4fd684f963a42a8b9951514466729ca147fef153
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-12-19 02:53:10 -0200

    tableviewcontroller working

--- MyWeight/WeightViewController.swift
+++ MyWeight/WeightViewController.swift
@@ -9,28 +9,26 @@
 import UIKit
 
 
-class WeightViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class WeightViewController: UITableViewController {
+    
+    var words: [String] = ["Android", "Is", "More", "Easy"]
+    
+    
+    // MARK: table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        cell.textLabel?.text = self.words[indexPath.row]
+        
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.words.count
     }
-    */
 
 }
